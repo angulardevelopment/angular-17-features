0c0486882edebbca8eb538c0df7fa30439c5d5cb
Gupta, Sunny <sunnyvales789@gmail.com>
2024-04-29 08:12:23 +0530

    update

--- src/app/large/large.component.ts
+++ src/app/large/large.component.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-    selector: 'app-large',
-    standalone: true,
-    imports: [
-        CommonModule,
-    ],
-    template: `<p>large works!</p>`,
-    styleUrl: './large.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-large',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  template: `<p>large works!</p>`,
+  styleUrl: './large.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LargeComponent { }
+export class LargeComponent {
+
+  constructor(public api: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.apiCall();
+  }
+
+  apiCall() {
+    this.api.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe((res) => {
+      console.log(res);
+    })
+  }
+}
