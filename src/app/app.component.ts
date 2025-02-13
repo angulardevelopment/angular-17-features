50cc5f2d2f7f50f7d014869d75299b036203ea1d
Gupta, Sunny <sunnyvales789@gmail.com>
2025-02-13 10:28:42 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet, Routes } from '@angular/router';
 import { ControlFlowComponent } from './control-flow/control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ControlFlowComponent],
+  imports: [CommonModule, RouterOutlet, ControlFlowComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,4 +14,16 @@ export class AppComponent {
   ngOnInit(){
     console.log(this)
   }
+
+  routes: Routes = [];
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    // Fetch the routes from the router
+    this.routes = this.router.config;
+    console.log(this.routes);
+  }
+
+
 }
